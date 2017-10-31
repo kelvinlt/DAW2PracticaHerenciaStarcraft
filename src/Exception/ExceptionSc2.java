3f842ff65e4cf6f0b2ac1cf109c3bb23d23ed7db
Kelvin <Kelvinleethen@gmail.com>
2017-10-31 11:44:01 +0100

    Creacion de las exceptions en el documento de exception

--- src/Exception/ExceptionSc2.java
+++ src/Exception/ExceptionSc2.java
@@ -9,6 +9,34 @@ package Exception;
  *
  * @author DAM
  */
-public class ExceptionSc2 {
+public class ExceptionSc2 extends Exception {
+    private int code;
+    public ExceptionSc2(int code) {
+        super(); 
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        switch(this.code){
+            case 1: 
+                return "< ERROR 001: Nº de argumentos inválido > ";
+            case 2: 
+                return "< ERROR 002: Especie incorrecta > ";              
+            case 3:   
+                return "< ERROR 003: Dato incorrecto >";
+            case 4:
+                return "< ERROR 004: Operación incorrecta >";
+            case 5:
+                return "< ERROR 005: No existe especie con ese nombre >";
+            case 6:
+                return "< ERROR 006: Propiedad incorrecta>";
+            case 7:
+                return "< ERROR 007: Ya existe un escuadrón con ese nombre >";
+        }
+        return null;
+    }
+    
+    
     
 }
